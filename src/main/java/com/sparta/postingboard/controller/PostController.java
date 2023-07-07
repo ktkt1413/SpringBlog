4002fb3f9bb6f350e12e206338ad446a44a1b833
JungDaGyeom <ktkt1413@naver.com>
2023-07-07 13:26:14 +0900

    커밋 테스트3

--- src/main/java/com/sparta/postingboard/controller/PostController.java
+++ src/main/java/com/sparta/postingboard/controller/PostController.java
@@ -37,7 +37,7 @@ public class PostController {
         postService.updatePost(id, postRequestDto);
     }
 
-  // 깃허브 커밋 완료 1
+  // 깃허브 커밋 완료 234
     @DeleteMapping("/posts/{id}")
     public void deletePost(@PathVariable Long id, @RequestBody PostRequestDto postRequest){
         postService.deletePost(id, postRequest);
